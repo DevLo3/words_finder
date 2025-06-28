ceae953e84c8dd3bdd92e8561b824936807742aa
DevLo3 <98679687+DevLo3@users.noreply.github.com>
2025-06-28 16:08:05 -0500

    display line number alongside program results

--- helpers.py
+++ helpers.py
@@ -5,6 +5,7 @@ colors = {
     'base': "\033[0m"
 }
 
+
 def get_files_path(extension='.txt'):
     # create a string that is the path to words_dir directory
     files_path = os.path.join(os.curdir, 'words_dir')
@@ -22,9 +23,9 @@ def find_word_in_files(str_to_find):
     for file in all_files:
         with open(file, 'r') as f:
             lines = f.readlines()
-            for line in lines:
+            for line_num, line in enumerate(lines):
                 if str_to_find in line:
                     matched_lined_pretty = line.replace(
                         str_to_find, f"{colors['red']}{str_to_find}{colors['base']}"
                     )
-                    print(f"{file}:{matched_lined_pretty}")
+                    print(f"{file}, line #{line_num + 1}:{matched_lined_pretty}")
