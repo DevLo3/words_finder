958a22c30d352a31de3e495ee52fd3d75bae558d
DevLo3 <98679687+DevLo3@users.noreply.github.com>
2025-06-28 15:53:26 -0500

    allow program to receive input from the command line

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from helpers import *
+import sys
 
 # main execution block/script entry point
 if __name__ == '__main__':
-    find_word_in_files(str_to_find='jim')
+    # assigns value of the 2nd argument passed in the command line to str_to_find
+    str_to_find = sys.argv[1]
+
+    # passes command line argument to the find_word_in_files() function
+    find_word_in_files(str_to_find=str_to_find)
