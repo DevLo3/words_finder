e1e3848c38655e20f44c46c6831a227891a7ce69
DevLo3 <98679687+DevLo3@users.noreply.github.com>
2025-06-28 15:58:10 -0500

    add try & except blocks to main.py

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import sys
 
 # main execution block/script entry point
 if __name__ == '__main__':
-    # assigns value of the 2nd argument passed in the command line to str_to_find
-    str_to_find = sys.argv[1]
+    try:
+        # assigns value of the 2nd argument passed in the command line to str_to_find
+        str_to_find = sys.argv[1]
 
-    # passes command line argument to the find_word_in_files() function
-    find_word_in_files(str_to_find=str_to_find)
+        # passes command line argument to the find_word_in_files() function
+        find_word_in_files(str_to_find=str_to_find)
+    except IndexError:
+        print(
+            "Please provide an argument in the execution.\n"
+            "Example: python main.py foo"
+        )
